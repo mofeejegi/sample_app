3c33b05a45330e130d76176ffcedb63622700903
Mofe Ejegi <ayeoritsemofe.ejegi@branch.co>
2020-07-30 22:53:16 +0100

    Completed email setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_013803) do
+ActiveRecord::Schema.define(version: 2020_07_30_173723) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
